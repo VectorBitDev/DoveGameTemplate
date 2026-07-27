ce53aea635d8116b49aaf47523d26b428c0d9a6d
Samuel <samuelsmendes2008@gmail.com>
2026-07-27 14:49:30 +0100

    initial commit

---
+++ src/Program.cs
@@ -0,0 +1,39 @@
+﻿using DoveCanvas;
+
+namespace DoveGameTemplate;
+
+/**
+    * @class Program
+    * @brief The main entry point for the application.
+*/
+public class Program
+{
+    /**
+        * @brief The main entry point for the application.
+        * @param args Command line arguments.
+    */
+    public static void Main(string[] args)
+    {
+        var WindowConfig = new WindowConfiguration()
+        {
+            Title = "Dove Template",
+        };
+
+
+        var app = new DoveCanvasApplication();
+        app.SetDefaultScene(new TestScene());
+        app.Initialize(WindowConfig);
+        app.Run(() =>
+        {
+            Logger.Info("Application is running.");
+        },
+        () =>
+        {
+            Logger.Info("Application is shutting down.");
+        },
+        () =>
+        {
+            Logger.Info("Appliaction Crashed!.");
+        });
+    }
+}
---
+++ src/scene.cs
@@ -0,0 +1,41 @@
+using DoveCanvas.Abstract;
+using DoveCanvas;
+using Raylib_cs;
+
+namespace DoveGameTemplate;
+
+/**
+    * @class TestScene
+    * @brief A test scene for the DoveCanvas framework.
+*/
+public class TestScene : Scene
+{
+    /**
+        * @brief Loads the resources and initializes the scene.
+    */
+    public override void Load()
+    {
+    }
+
+    /**
+        * @brief Unloads the resources and cleans up the scene.
+    */
+    public override void Unload()
+    {
+    }
+
+    /**
+        * @brief Updates the scene logic.
+        * @param dt The time elapsed since the last frame.
+    */
+    public override void Tick(float dt)
+    {
+    }
+
+    /**
+        * @brief Draws the scene objects.
+    */
+    public override void Draw()
+    {
+    }
+}
